46e4e6b5864fc71b369ae58e6edc1d5a8eebe0e8
Alexander <alexander.advanced@gmail.com>
2024-03-23 12:20:13 +0100

    - update

--- src/Twig/MenuRenderExtension.php
+++ src/Twig/MenuRenderExtension.php
@@ -2,10 +2,13 @@
 
 namespace AlexanderA2\PlatformBundle\Twig;
 
+use AlexanderA2\PlatformBundle\Event\MenuBuildEvent;
 use Knp\Menu\ItemInterface;
 use Knp\Menu\Matcher\Matcher;
+use Knp\Menu\MenuFactory;
 use Knp\Menu\Renderer\ListRenderer;
 use Knp\Menu\Renderer\TwigRenderer;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
@@ -13,16 +16,26 @@ use Twig\TwigFunction;
 class MenuRenderExtension extends AbstractExtension
 {
     public function __construct(
-        protected Environment $twig,
+        protected EventDispatcherInterface $eventDispatcher,
+        protected Environment              $twig,
     ) {
     }
 
     public function getFunctions(): array
     {
         return [
+            new TwigFunction('build_menu', [$this, 'buildMenu'], ['is_safe' => ['html']]),
             new TwigFunction('render_menu', [$this, 'renderMenu'], ['is_safe' => ['html']]),
             new TwigFunction('render_sidebar_menu', [$this, 'renderSidebarMenu'], ['is_safe' => ['html']]),
         ];
+    }
+
+    public function buildMenu(string $name): ItemInterface
+    {
+        $event = new MenuBuildEvent($name, (new MenuFactory())->createItem($name));
+        $this->eventDispatcher->dispatch($event);
+
+        return $event->getMenu();
     }
 
     public function renderMenu(ItemInterface $menu): string
